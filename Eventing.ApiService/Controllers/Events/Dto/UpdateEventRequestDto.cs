10a8de309dcdf7ab45c97cbda91782d788161ea8
Manas Dongol <manasdongol27@gmail.com>
2025-07-27 20:05:56 +0545

    updates and changes made to the events dto

--- Eventing.ApiService/Controllers/Events/Dto/UpdateEventRequestDto.cs
+++ Eventing.ApiService/Controllers/Events/Dto/UpdateEventRequestDto.cs
@@ -1,6 +1,37 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Eventing.ApiService.Controllers.Events.Dto;
 
-public class UpdateEventRequestDto_
-{
+public sealed record UpdateEventRequestDto_
+(
+    [Required]
+    [MaxLength(64)]
+    [RegularExpression("^([A-Z][a-z]+)( [A-Z][a-z]+)*$",ErrorMessage = "The Full Name field is not in a valid format.")]
+    [property: Description("The event title")]
+    string Title,
+        
+    [Required]
+    [property: Description("The event location")]
+    string Location,
+
+    [Required]
+    [MaxLength(128)]
+    [property: Description("The event description")]
+    string EventDescription,
     
-}
+    [Required]
+    [MaxLength(128)]
+    [property: Description("The event type")]
+    string EventType,
+    
+    [Required]
+    [property: Description("The event starttime")]
+    DateTime StartTime,
+    
+    [Required]
+    [property: Description("The event endtime")]
+    DateTime EndtTime
+    
+    
+    );
